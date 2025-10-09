21e27adf4d9b9dd392470cbced570454c04c3ea6
Crijthel <cristhel.francisco@davinci.edu.ar>
2025-10-09 19:02:53 -0300

    form funca

--- src/containers/contact/Contact.tsx
+++ src/containers/contact/Contact.tsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import Formw from '../../components/Form/Form'
+import React from "react";
+import Formw from "../../components/Form/Form";
+import ig from "../../assets/img/IG-01.avif";
+import art from "../../assets/img/ARTSTATIOn-01.avif";
+import "./Contact.scss";
 //import Form from "./Form";
 const Contact = () => {
-  return (
-     <div id="Contact">
-      <h1 className="title">Contact</h1>
-      <div className="contact_container">
-        <Formw/>
-        <div className="links">
-          <a
-            href="https://www.linkedin.com/in/andreaibt/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="assets/linkedin.svg" alt="" />
-          </a>
-          <a
-            href="https://codepen.io/andreaibt21"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="assets/codepen.svg" alt="" />
-          </a>
-          <a
-            href="https://github.com/andreaibt21"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="assets/github.svg" alt="" />
-          </a>
+    return (
+        <div id="Contact">
+            <h1 className="title">Contacto</h1>
+            <div className="contact_container">
+                <Formw />
+                <div className="links">
+                    <a
+                        href="https://www.instagram.com/peregrina_dm/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={ig} alt="" />
+                    </a>
+                    <a
+                        href="https://www.artstation.com/peregrina"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={art} alt="" />
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>  
-  )
-}
+    );
+};
 
-export default Contact
+export default Contact;
