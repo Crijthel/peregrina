f8a16559b4c2331fa7d506361d60f9149770f161
Crijthel <cristhel.francisco@davinci.edu.ar>
2025-12-16 23:03:48 -0300

    agregue la multimedia + falta responsiveness + flecha hacia arriba

--- src/containers/Identidad/Identidad.tsx
+++ src/containers/Identidad/Identidad.tsx
@@ -1,88 +1,94 @@
-import React, { useState } from 'react';
-import loading from '../../assets/loading.svg';
-import './Identidad.scss';
+import React, { useState } from "react";
+import loading from "../../assets/loading.svg";
+import "./Identidad.scss";
 
 const Identidad = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isLoading2, setIsLoading2] = useState(true);
-  const [isLoading3, setIsLoading3] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading2, setIsLoading2] = useState(true);
+    const [isLoading3, setIsLoading3] = useState(true);
 
-  const handleIframeLoad = () => {
-    setIsLoading(false);
-  };
+    const handleIframeLoad = () => {
+        setIsLoading(false);
+    };
 
     const handleIframeLoad2 = () => {
-    setIsLoading2(false);
-  };
+        setIsLoading2(false);
+    };
     const handleIframeLoad3 = () => {
-    setIsLoading3(false);
-  };
-  return (
-    <div className='container-identidad'>
-      <h1>Identidad</h1>
+        setIsLoading3(false);
+    };
+    return (
+        <div className="container-identidad">
+            <h1>Identidad</h1>
 
-      <div>
-        <h2>MANIFIESTO</h2>
+            <div className="item-identidad">
+                <h2>Manifiesto</h2>
 
-        <div className='video-id'>
-          {isLoading && <img src={loading} className='loading' alt='loading' />}
-          <iframe
-            loading='eager'
-            src={
-              'https://www.youtube.com/embed/rDM6TjKru2k?si=fCuqGWN2JH_QxIuy&loop=1&playlist=rDM6TjKru2k&autoplay=1&muted=1'
-            }
-            title={'MANIFIESTO'}
-            allowTransparency
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            referrerPolicy='strict-origin-when-cross-origin'
-            allowFullScreen
-            onLoad={handleIframeLoad}
-            style={{ display: isLoading ? 'none' : 'block' }}
-          ></iframe>
+                <div className="video-id">
+                    {isLoading && (
+                        <img src={loading} className="loading" alt="loading" />
+                    )}
+                    <iframe
+                        loading="eager"
+                        src={
+                            "https://www.youtube.com/embed/bdo0WmerAP0?si=BnNmF5kwqlVYdO6s&loop=1&playlist=bdo0WmerAP0&autoplay=1&muted=1"
+                        }
+                        title={"MANIFIESTO"}
+                        allowTransparency
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                        onLoad={handleIframeLoad}
+                        style={{ display: isLoading ? "none" : "block" }}
+                    ></iframe>
+                </div>
+            </div>
+            <div className="item-identidad">
+                <h2>Brandboard</h2>
+
+                <div className="video-id">
+                    {isLoading2 && (
+                        <img src={loading} className="loading" alt="loading" />
+                    )}
+                    <iframe
+                        loading="eager"
+                        src={
+                            "https://www.youtube.com/embed/IA-IGhmg_XU?si=-JABe0s0oZYVCiFI&loop=1&playlist=IA-IGhmg_XU"
+                        }
+                        title={"BRANDBOARD"}
+                        allowTransparency
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                        onLoad={handleIframeLoad2}
+                        style={{ display: isLoading2 ? "none" : "block" }}
+                    ></iframe>
+                </div>
+            </div>
+            <div className="item-identidad">
+                <h2>Demoreel</h2>
+
+                <div className="video-id">
+                    {isLoading3 && (
+                        <img src={loading} className="loading" alt="loading" />
+                    )}
+                    <iframe
+                        loading="eager"
+                        src={
+                            "https://www.youtube.com/embed/cc2b8HGzmZc?si=agtxHEqQcTGy9dIz&loop=1&playlist=cc2b8HGzmZc"
+                        }
+                        title={"DEMOREEL"}
+                        allowTransparency
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                        onLoad={handleIframeLoad3}
+                        style={{ display: isLoading3 ? "none" : "block" }}
+                    ></iframe>
+                </div>
+            </div>
         </div>
-      </div>
-      <div>
-        <h2>BRANDBOARD</h2>
-
-        <div className='video-id'>
-          {isLoading2 && <img src={loading} className='loading' alt='loading' />}
-          <iframe
-            loading='eager'
-            src={
-              'https://www.youtube.com/embed/rDM6TjKru2k?si=fCuqGWN2JH_QxIuy&loop=1&playlist=rDM6TjKru2k&autoplay=1'
-            }
-            title={'BRANDBOARD'}
-            allowTransparency
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            referrerPolicy='strict-origin-when-cross-origin'
-            allowFullScreen
-            onLoad={handleIframeLoad2}
-            style={{ display: isLoading2 ? 'none' : 'block' }}
-          ></iframe>
-        </div>
-      </div>
-      <div>
-        <h2>DEMOREEL</h2>
-
-        <div className='video-id'>
-          {isLoading3 && <img src={loading} className='loading' alt='loading' />}
-          <iframe
-            loading='eager'
-            src={
- 'https://www.youtube.com/embed/rDM6TjKru2k?si=fCuqGWN2JH_QxIuy&loop=1&playlist=rDM6TjKru2k&autoplay=1'
-            }
-            title={'DEMOREEL'}
-            allowTransparency
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            referrerPolicy='strict-origin-when-cross-origin'
-            allowFullScreen
-            onLoad={handleIframeLoad3}
-            style={{ display: isLoading3 ? 'none' : 'block' }}
-          ></iframe>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Identidad;
--- src/containers/Nosotras/Nosotras.tsx
+++ src/containers/Nosotras/Nosotras.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
- 
+import React from "react";
+import "./Nosotras.scss";
+import personaje1 from "../../assets/proyectos/personajes/personajes1.png";
+import personaje2 from "../../assets/proyectos/personajes/personajes2.png";
+import personaje3 from "../../assets/proyectos/personajes/personajes3.png";
+import personaje4 from "../../assets/proyectos/personajes/personajes4.png";
+import personaje5 from "../../assets/proyectos/personajes/personajes5.png";
+import personaje6 from "../../assets/proyectos/personajes/personajes6.png";
+import personaje7 from "../../assets/proyectos/personajes/personajes7.png";
 const Nosotras = () => {
-  return (
+    return (
+        <div className="container-nosotras">
+            <h1>NOSOTRAS</h1>
 
-      <div className='container-nosotras'>nosotrassssss</div>
-  );
+            <div className="contenido">
+                <div className="textos">
+                    <span>
+                        Nuestra realidad se crea en BASE a nuestro impulso
+                        curioso y la búsqueda de lo extraordinario.
+                    </span>
+                    <span>“Caminamos a la par de una mente curiosa”</span>
+                    <span>
+                        Somos una agencia que genera productos más allá de la
+                        imaginación. Todos tenemos una historia que contar,
+                        nosotras nos encargamos de traerla a la realidad sin que
+                        pierda su esencia original.
+                    </span>
+                </div>
+                <div className="personajes">
+                    <img src={personaje1} alt="personaje" />
+                    <img src={personaje2} alt="personaje" />
+                    <img src={personaje3} alt="personaje" />
+                    <img src={personaje4} alt="personaje" />
+                    <img src={personaje5} alt="personaje" />
+                    <img src={personaje6} alt="personaje" />
+                    <img src={personaje7} alt="personaje" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Nosotras;
